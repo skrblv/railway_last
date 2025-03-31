f3736bca55fc55c158335d5ed64be37fb8628ad3
skrblv <asylbekmaratovic98@gmail.com>
2025-03-31 12:02:40 +0600

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,8 @@
 # backend/urls.py
 from django.contrib import admin
-from django.urls import path, include # Make sure 'include' is imported
-from api import views  # Добавьте этот импорт
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')), # Add this line to include your app's URLs under /api/
-    # You might add paths for serving the frontend later
+    path('api/', include('api.urls')),  # Подключаем URL-ы API приложения
 ]
