50c7fb06e7878704702ff9c65b64c5e475d20bf8
skrblv <asylbekmaratovic98@gmail.com>
2025-04-01 14:54:45 +0600

    Update venue-detail.js

--- frontend/venue-detail.js
+++ frontend/venue-detail.js
@@ -2,7 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     const venueDetailContainer = document.getElementById('venue-detail-content');
-    const API_BASE_URL = 'http://127.0.0.1:8000/api'; // Your Django API base URL
+    const API_BASE_URL = "/api";
 
     // --- Get Venue ID from URL Parameter ---
     const urlParams = new URLSearchParams(window.location.search);
