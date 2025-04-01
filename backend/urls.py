1fa53c5c481dc376648f4a41766386b182764baf
skrblv <asylbekmaratovic98@gmail.com>
2025-04-01 12:44:54 +0600

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,13 @@
-# backend/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render  # Добавляем render для шаблонов
+
+# Представление для рендеринга HTML-страницы
+def home(request):
+    return render(request, "index.html")  # Покажет index.html из frontend/assets
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Подключаем URL-ы API приложения
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    path("", home),  # Открываем HTML на главной странице
 ]
