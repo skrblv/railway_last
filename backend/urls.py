9cc5867c635e6f7fdafd7437ef005fbbe055859b
skrblv <asylbekmaratovic98@gmail.com>
2025-04-03 18:29:08 +0600

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,15 +1,9 @@
-# backend/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
-from api import views as api_views  # Импортируйте ваши представления из api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Включаем API маршруты
-
-    # Frontend Pages
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),  # Главная страница
-
+    path('api/', include('api.urls')), # Оставляем только API и админку
 
 ]
+
