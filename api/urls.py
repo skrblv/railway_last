b528445757d05be4c865b54e2a7ece41b8a16bd8
skrblv <asylbekmaratovic98@gmail.com>
2025-04-03 19:24:39 +0600

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,14 @@
 # api/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import VenueViewSet # Only import VenueViewSet
+from .views import VenueViewSet, PlanViewSet
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'venues', VenueViewSet, basename='venue') # URL prefix: /api/venues/
-# --- Remove the registration for plans ---
-# router.register(r'plans', PlanViewSet, basename='plan')
+router.register(r'plans', PlanViewSet, basename='plan')     # URL prefix: /api/plans/
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
     path('', include(router.urls)),
-    path('venue/<int:pk>/', api_views.venue_detail_view, name='venue-detail'),
-
 ]
