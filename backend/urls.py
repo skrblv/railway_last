49cbf23ed4325b8a5f6c78769ca3831f8b51ad70
skrblv <asylbekmaratovic98@gmail.com>
2025-04-03 18:40:34 +0600

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')), # Оставляем только API и админку
-
+    path('api/', include('api.urls')), # Оставляем ТОЛЬКО API и админку
 ]
+
+# Если у вас есть настройки для статики в DEBUG, они могут остаться,
+# но строки path('', ...) и path('venue/<int:pk>/', ...) быть НЕ ДОЛЖНО.
