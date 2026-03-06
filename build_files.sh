971465ef449e6fd0593dc5069ddb7f38ec6b77bb
skrblv <asylbekmaratovic98@gmail.com>
2026-03-06 22:25:23 +0600

    change: static

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,14 @@
+# Создаем виртуальное окружение
+python3.12 -m venv venv
+# Активируем его
+source venv/bin/activate
+
+# Устанавливаем зависимости
+pip install --upgrade pip
 pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput
+
+# Собираем статику
+python3.12 manage.py collectstatic --noinput --clear
+
+# Деактивируем окружение
+deactivate
